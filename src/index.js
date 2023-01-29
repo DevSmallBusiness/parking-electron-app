b7b529b7b08044b0b718b0b685735f0bf1fa4c8d
Brayan Gómez <brayan.gomez@sofka.com.co>
2023-01-29 16:17:55 -0500

    the app was configured

--- src/index.js
+++ src/index.js
@@ -1,74 +1,41 @@
-const { app, BrowserWindow, Menu, ipcMain } = require("electron");
-
-const url = require("url");
+const { app, BrowserWindow, Menu } = require("electron");
 const path = require("path");
 
-let mainWindow;
-let newProductWindow;
-
-// Reload in Development for Browser Windows
 if (process.env.NODE_ENV !== "production") {
-	require("electron-reload")(__dirname, {
-		electron: path.join(__dirname, "../node_modules", ".bin", "electron"),
-	});
+  require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "../node_modules", ".bin", "electron"),
+  });
 }
 
-app.on("ready", () => {
-	// The Main Window
-	mainWindow = new BrowserWindow({
-		width: 720,
-		height: 600,
-		webPreferences: {
-			nodeIntegration: true,
-			contextIsolation: false,
-			enableRemoteModule: true,
-		},
-	});
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
+  });
 
-	mainWindow.loadURL(
-		url.format({
-			pathname: path.join(__dirname, "views/index.html"),
-			protocol: "file",
-			slashes: true,
-		})
-	);
+  win.loadFile("views/index.html");
 
-	// Menu
-	const mainMenu = Menu.buildFromTemplate(templateMenu);
-	// Set The Menu to the Main Window
-	Menu.setApplicationMenu(mainMenu);
+  Menu.setApplicationMenu(null);
+}
 
-	// If we close main Window the App quit
-	mainWindow.on("closed", () => {
-		app.quit();
-	});
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
 });
 
-// Menu Template
-const templateMenu = [
-	{
-		label: "File",
-		submenu: [
-			{
-				label: "New Product",
-				accelerator: "Ctrl+N",
-				click() {
-					createNewProductWindow();
-				},
-			},
-			{
-				label: "Remove All Products",
-				click() {
-					mainWindow.webContents.send("products:remove-all");
-				},
-			},
-			{
-				label: "Exit",
-				accelerator: process.platform == "darwin" ? "command+Q" : "Ctrl+Q",
-				click() {
-					app.quit();
-				},
-			},
-		],
-	},
-];
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
